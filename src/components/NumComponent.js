2b7e265733baae1ed4af54025bf340048a7ef170
wangaijun <hw_wangaijun@163.com>
2019-08-21 20:13:43 +0800

    解决线偏移的问题

--- src/components/NumComponent.js
+++ src/components/NumComponent.js
@@ -5,10 +5,17 @@ class NumComponent extends Rete.Component {
         super('工作任务项');
         this.editor = editor
         this.vueComponent = vueComponent
+        this.numSocket = numSocket
     }
 
     builder(node) {
-        node.addControl(new NumControl(this.editor, 'num', this.vueComponent));
+        var inp1 = new Rete.Input('num1',"输入", this.numSocket, true); // 下同，第四个参数表示允许多个连接
+        var out = new Rete.Output('num2', "输出", this.numSocket, true);
+        inp1.addControl(new NumControl(this.editor, 'num'))
+        return node
+            .addInput(inp1)
+            .addControl(new NumControl(this.editor, 'num', this.vueComponent))
+            .addOutput(out)
     }
 
     worker(node, inputs, outputs){
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="home">
-    <div id="rete" class="node-editor"></div>
-    <div>{{ret}}</div>
-  </div>
+
+    <div id="rete"></div>
+<!--    <div>{{ret}}</div>-->
+
 </template>
 
 <script>
@@ -24,7 +24,13 @@ export default {
     const container = document.querySelector('#rete');
 
     const editor = new Rete.NodeEditor('demo@0.1.0', container);
+    editor.use(ConnectionPlugin);
     editor.use(VueRenderPlugin)
+    editor.use(ContextMenuPlugin.default);
+    editor.use(AreaPlugin);
+    editor.use(CommentPlugin.default);
+    editor.use(HistoryPlugin);
+    editor.use(ConnectionMasteryPlugin.default);
 
     const numComponent = new NumComponent(editor, VueNumControl, numSocket);
 
@@ -37,10 +43,16 @@ export default {
 
       const n1 = await numComponent.createNode({num: 2});
       const n2 = await numComponent.createNode({num: 4});
-      n1.position = [80,200];
-      n2.position = [80, 400];
+      const n3 = await numComponent.createNode({num: 8});
+      n1.position = [0,200];
+      n2.position = [380, 400];
+      n3.position = [500, 240]
       editor.addNode(n1)
       editor.addNode(n2)
+      editor.addNode(n3)
+
+      editor.connect(n1.outputs.get('num2'), n2.inputs.get('num1'));
+      editor.connect(n2.outputs.get('num2'), n3.inputs.get('num1'));
 
       editor.on('process nodecreated noderemoved connectioncreated connectionremoved', async () => {
         await engine.abort();
@@ -65,13 +77,18 @@ export default {
 </script>
 
 <style>
+  html, body {
+    height: 100%;
+    width: 100%;
+  }
   .socket.number{
     background: #96b38a
   }
-  .home {
-
-  }
-  .home .node-editor{
-    height: 400px;
-  }
+  /*.home {*/
+  /*  width: 100%;*/
+  /*  height: 100%;*/
+  /*}*/
+  /*.home .node-editor{*/
+  /*  !*height: 800px;*!*/
+  /*}*/
 </style>
